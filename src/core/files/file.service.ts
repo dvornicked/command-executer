3f3c7ed595898d29324f832744e2cd469e9eb357
Dvornick <mail@dvornick.ru>
2022-07-21 00:53:38 +0300

    Implement file handler

---
+++ src/core/files/file.service.ts
@@ -0,0 +1,24 @@
+import { promises } from 'fs'
+import { dirname, isAbsolute, join } from 'path'
+
+export class FileService {
+  private async isExists(path: string) {
+    try {
+      await promises.stat(path)
+      return true
+    } catch {
+      return false
+    }
+  }
+
+  getFilePath(path: string, name: string, ext: string): string {
+    if (!isAbsolute(path)) path = join(__dirname + '/' + path)
+    return join(dirname(path) + '/' + name + '.' + ext)
+  }
+
+  async deleteFileIfExists(path: string) {
+    if (await this.isExists(path)) {
+      promises.unlink(path)
+    }
+  }
+}
